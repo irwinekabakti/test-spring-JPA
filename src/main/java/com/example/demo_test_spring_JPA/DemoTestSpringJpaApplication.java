54ed32fb957bc163cde81cb6f7f1a0db2278cacc
irwinekabakti <irwinekabakti@gmail.com>
2024-05-30 13:06:18 +0700

    initial commit

---
+++ src/main/java/com/example/demo_test_spring_JPA/DemoTestSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_test_spring_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_test_spring_JPA/DemoTestSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_test_spring_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
