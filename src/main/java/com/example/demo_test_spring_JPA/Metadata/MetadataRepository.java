e1df7479326ae3d6d4a21df409abfe01a0effa45
irwinekabakti <irwinekabakti@gmail.com>
2024-05-30 19:38:07 +0700

    fix:(Metadata Repository)

--- src/main/java/com/example/demo_test_spring_JPA/Metadata/MetadataRepository.java
+++ src/main/java/com/example/demo_test_spring_JPA/Metadata/MetadataRepository.java
@@ -1,7 +1,12 @@
 package com.example.demo_test_spring_JPA.Metadata;
 
-import com.example.demo_test_spring_JPA.Products.Products;
+//import com.example.demo_test_spring_JPA.Products.Products;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface MetadataRepository extends CrudRepository<Products, Integer> {
+import java.util.Optional;
+
+@Repository
+public interface MetadataRepository extends CrudRepository<Metadata, Integer> {
+    Optional<Metadata> findByProductId(int id);
 }
